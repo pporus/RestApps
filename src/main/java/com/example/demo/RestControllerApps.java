c08abedf7a2de51d774ac64de2dc5d5ace1029f4
Admin <puruaws@gmail.com>
2022-05-27 22:31:41 +0530

    Modified RestController with /name

--- src/main/java/com/example/demo/RestControllerApps.java
+++ src/main/java/com/example/demo/RestControllerApps.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.Date;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class RestControllerApps {
 	{
 		return "Welcome to Jenkins RestApps  "+new Date();
 	}
+	
+	@GetMapping("/{name}")
+	public String  getName(@PathVariable String name)
+	{
+		return "Welcome Mr."+name+ " "+ new Date();
+	}
+
 
 }
